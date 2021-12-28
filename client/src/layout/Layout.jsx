764df85eb90e2faf3ad1c4b735c1d507fbf90dca
Paige Hoeppner <paige.a.hoeppner@gmail.com>
2021-12-28 16:40:32 -0600

    home base styling

--- client/src/layout/Layout.jsx
+++ client/src/layout/Layout.jsx
@@ -1,36 +1,72 @@
 // Packages
-import { NavHashLink } from 'react-router-hash-link';
+import { NavHashLink } from "react-router-hash-link";
 
 // CSS
-import './Layout.css';
+import "./Layout.css";
 
 // Assets
-import githubLogo from '../assets/graphics/github.svg';
-import linkedinLogo from '../assets/graphics/linkedin.svg';
-import homeButtonCorners from '../assets/graphics/home-button-corners.png'
-import resumePdf from '../assets/resume/paige-hoeppner-resume.pdf';
+import githubLogo from "../assets/graphics/github.svg";
+import linkedinLogo from "../assets/graphics/linkedin.svg";
+import resumePdf from "../assets/resume/paige-hoeppner-resume.pdf";
 
 export default function Layout({ children }) {
   return (
     <div className="layout">
       <nav>
-        {/* <img className="home-button-corners" src={homeButtonCorners} alt="home button corners" /> */}
-        <NavHashLink className="home-link" smooth to="#top">Home</NavHashLink>
+        <NavHashLink
+          className="home-link"
+          smooth
+          to="#top"
+          activeClassName="selected"
+          activeStyle={{ color: "red" }}
+        >
+          Home
+        </NavHashLink>
         <div className="section-links">
-          <NavHashLink className="nav-link" smooth to="#about">About</NavHashLink>
-          <NavHashLink className="nav-link" smooth to="#projects" activeClassName="selected" activeStyle={{ color: 'red' }}>Projects</NavHashLink>
-          <NavHashLink className="nav-link" smooth to="#contact">Contact</NavHashLink>
-          <a className="nav-link" href={resumePdf} target="_blank">Resume</a>
+          <NavHashLink
+            className="nav-link"
+            smooth
+            to="#about"
+            activeClassName="selected"
+            activeStyle={{ color: "red" }}
+          >
+            About
+          </NavHashLink>
+          <NavHashLink
+            className="nav-link"
+            smooth
+            to="#projects"
+            activeClassName="selected"
+            activeStyle={{ color: "red" }}
+          >
+            Projects
+          </NavHashLink>
+          <NavHashLink
+            className="nav-link"
+            smooth
+            to="#contact"
+            activeClassName="selected"
+            activeStyle={{ color: "red" }}
+          >
+            Contact
+          </NavHashLink>
+          <button>
+            <a className="nav-link" href={resumePdf} target="_blank">
+              Resume
+            </a>
+          </button>
         </div>
       </nav>
-      <main>
-        {children}
-      </main>
+      <main>{children}</main>
       <footer>
-        <a href="https://www.linkedin.com/in/paige-hoeppner/" target="_blank"><img src={githubLogo} alt="Github Logo" /></a>
-        <a href="https://github.com/thepaiger" target="_blank"><img src={linkedinLogo} alt="LinkedIn Logo" /></a>
+        <a href="https://www.linkedin.com/in/paige-hoeppner/" target="_blank">
+          <img src={githubLogo} alt="Github Logo" />
+        </a>
+        <a href="https://github.com/thepaiger" target="_blank">
+          <img src={linkedinLogo} alt="LinkedIn Logo" />
+        </a>
         Designed & Built by Paige Hoeppner | 2021
       </footer>
     </div>
-  )
+  );
 }
--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -4,8 +4,10 @@ import './Home.css'
 export default function Home() {
   return (
     <section className="home" id="home">
-      <h1 className="main-heading">Hi, I'm Paige Hoeppner, software engineer</h1>
-      <h3>I love making work this is visually compelling and user-friendly.</h3>
+      <div className="intro-container">
+        <h1 className="intro-heading">Hi, I'm Paige Hoeppner, software engineer.</h1>
+        <h3 className="intro-subheading">I love making work this is visually compelling and user-friendly.</h3>
+      </div>
     </section>
   )
 }
