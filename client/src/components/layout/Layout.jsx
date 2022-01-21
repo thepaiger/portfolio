6499b72112bc3c084f828a3eee32e745196aacd3
Paige Hoeppner <paige.a.hoeppner@gmail.com>
2022-01-21 17:58:06 -0600

    removed footer code from Layout files

--- client/src/components/layout/Layout.jsx
+++ client/src/components/layout/Layout.jsx
@@ -1,6 +1,5 @@
 // ====================IMPORTS====================
 // Packages
-// import { useState, useEffect } from "react";
 import { NavHashLink } from "react-router-hash-link";
 
 // Components
@@ -18,19 +17,6 @@ import { logos } from "../../services/logos";
 // ====================NAV & FOOTER TO WRAP MAIN CONTENT====================
 
 export default function Layout({ children }) {
-  // USESTATE, USEEFFECT FOR SCROLL-TO-TOP BUTTON
-  // const [showScrollBtn, setShowScrollBtn] = useState(false);
-
-  // useEffect(() => {
-  //   window.addEventListener("scroll", () => {
-  //     if (window.pageYOffset > 300) {
-  //       setShowScrollBtn(true);
-  //     } else {
-  //       setShowScrollBtn(false);
-  //     }
-  //   });
-  // }, []);
-
   return (
     <div className="layout">
       {/* ==========MOBILE NAVBAR========== */}
@@ -105,22 +91,6 @@ export default function Layout({ children }) {
 
       {/* ==========FOOTER========== */}
       <Footer />
-
-
-      {/* <footer>
-        Designed & Built by Paige Hoeppner | 2021 */}
-      {/* Scroll-To-Top Button */}
-      {/* {showScrollBtn && (
-          <NavHashLink
-            className="scroll-up-btn"
-            smooth
-            to="#top"
-            activeClassName="selected"
-          >
-            {logos[3].svg}
-          </NavHashLink>
-        )}
-      </footer> */}
     </div>
   );
 }
