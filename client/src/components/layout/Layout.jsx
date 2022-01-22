2481dd79078ac664cfad4e35efc01914e95e0e50
Paige Hoeppner <paige.a.hoeppner@gmail.com>
2022-01-21 18:27:46 -0600

    set up NavHamburger files & Layout files

--- client/src/components/layout/Layout.jsx
+++ client/src/components/layout/Layout.jsx
@@ -1,35 +1,20 @@
 // ====================IMPORTS====================
-// Packages
-import { NavHashLink } from "react-router-hash-link";
-
 // Components
 import Footer from "../Footer/Footer"
+import NavDesktop from "../NavDesktop/NavDesktop";
+import NavHamburger from "../NavHamburger/NavHamburger";
 
 // CSS
 import "./Layout.css";
-
-// Assets
-import resumePdf from "../../assets/resume/paige-hoeppner-resume.pdf";
-
-// Services
-import { logos } from "../../services/logos";
-import NavDesktop from "../NavDesktop/NavDesktop";
 
 // ====================NAV & FOOTER TO WRAP MAIN CONTENT====================
 
 export default function Layout({ children }) {
   return (
     <div className="layout">
-      {/* ==========MOBILE NAVBAR========== */}
-      <nav className="nav-mobile"></nav>
-
-      {/* ==========DESKTOP NAVBAR========== */}
+      <NavHamburger />
       <NavDesktop />
-
-      {/* ==========MAIN CONTENT========== */}
       <main>{children}</main>
-
-      {/* ==========FOOTER========== */}
       <Footer />
     </div>
   );
